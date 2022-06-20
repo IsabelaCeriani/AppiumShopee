00324b4b1cbecca4977f637e260dab5dd92648d5
IsabelaCeriani <56088703+IsabelaCeriani@users.noreply.github.com>
2022-06-20 10:27:05 -0300

    Login added and set tests to run in sync

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,16 +1,29 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.PerformsTouchActions;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.WaitOptions.waitOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+import static io.appium.java_client.touch.offset.PointOption.point;
 import java.net.MalformedURLException;
 import java.net.URL;
 public class Test {
 
-    static AppiumDriver driver;
+    static AndroidDriver driver;
+    private static String name = "ll59d4iv79";
+    private static String password = "rcE6fnZtwMMHU.c";
+    private static String phoneNumb = "1138979854";
+
 
     public static void main(String[] args) {
         try {
             testSomething();
+            eliminatePopUps();
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
@@ -29,12 +42,23 @@ public class Test {
         capabilities.setCapability("appPackage", "com.shopee.ar");
         capabilities.setCapability("appActivity", "com.shopee.app.ui.home.HomeActivity_");
 
-        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         System.out.println("Appium driver initialized");
     }
 
-    public void testLogin(){
-        driver.findElement(By.id("00000000-0000-01c8-ffff-ffff0000002f")).click();
-        driver.findElement(By.id("00000000-0000-01c8-ffff-ffff000005d9")).click();
+    public static void eliminatePopUps(){
+        driver.findElement(By.id("com.android.permissioncontroller:id/permission_deny_button")).click();
+////        driver.findElement(By.id("android:id/ok")).click();
+//        timeLapse(4000);
+//        driver.findElement(By.id("com.shopee.ar:id/btn")).click();
+//        //wait 5 seconds
+//        timeLapse(9000);
+//        driver.findElement(By.id("com.android.permissioncontroller:id/permission_deny_button")).click();
+//
+
+
+
     }
+
+
 }

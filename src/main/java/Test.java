f5ec40c4cabe505477eec7bf434d8956229aca70
IsabelaCeriani <56088703+IsabelaCeriani@users.noreply.github.com>
2022-06-22 17:25:51 -0300

    Login added and set tests to run in sync

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,6 +5,7 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import static io.appium.java_client.touch.TapOptions.tapOptions;
 import static io.appium.java_client.touch.WaitOptions.waitOptions;
@@ -19,6 +20,7 @@ public class Test {
     private static String password = "rcE6fnZtwMMHU.c";
     private static String phoneNumb = "1138979854";
 
+    private static  TouchAction touchAction;
 
     public static void main(String[] args) {
         try {
@@ -43,7 +45,50 @@ public class Test {
         capabilities.setCapability("appActivity", "com.shopee.app.ui.home.HomeActivity_");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        touchAction=new TouchAction(driver);
         System.out.println("Appium driver initialized");
+
+        String dontAllow = "com.android.permissioncontroller:id/permission_deny_button";
+        String comenzarBtn = "com.shopee.ar:id/btn";
+        String notAllowGeoBtn = "com.android.permissioncontroller:id/permission_deny_button";
+        getBtn(dontAllow).click();
+//        driver.findElement(By.id("android:id/ok")).click();
+        timeLapse(5000);
+        getBtn(comenzarBtn).click();
+        timeLapse(7000);
+        getBtn(notAllowGeoBtn).click();
+
+
+
+        PointOption loginHomeBtn = PointOption.point(980, 1762);
+        PointOption loginSettingBtn = PointOption.point(935, 1774);
+        PointOption nameBtn = PointOption.point(265, 596);
+        String nameField = "com.shopee.ar:id/cret_edit_text";
+        PointOption passwordBtn = PointOption.point(233, 401);
+        PointOption loginSubmitBtn = PointOption.point(523, 900);
+        String passwordFieldxpath = "hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.view.ViewGroup[2]/android.widget.EditText";
+
+        System.out.println("1");
+        performTap(loginHomeBtn);
+        timeLapse(2000);
+        System.out.println("1");
+        performTap(loginSettingBtn);
+        timeLapse(2000);
+        System.out.println("2");
+        performTap(nameBtn);
+        timeLapse(3000);
+        System.out.println("3");
+        getBtn(nameField).sendKeys(new String[]{name});
+        timeLapse(2000);
+        System.out.println("4");
+        performTap(passwordBtn);
+        timeLapse(2000);
+        System.out.println("5");
+        driver.findElement(By.xpath(passwordFieldxpath)).sendKeys(new String[]{password});;
+        System.out.println("6");
+        timeLapse(2000);
+
+        performTap(loginSubmitBtn);
     }
 
     public static void eliminatePopUps(){
@@ -59,6 +104,21 @@ public class Test {
 
 
     }
+    private static TouchAction performTap(PointOption pointOption) {
+        return touchAction.tap(pointOption).perform();
+    }
+
+    private static WebElement getBtn(String id) {
+        return driver.findElement(By.id(id));
+    }
+    private static void timeLapse(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 }
